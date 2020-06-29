8ab9533069e66a40a464ffc9126a743904740bb8
Peter Rosenberg <p.rosenberg@gmx.net>
2020-06-29 16:57:24 +0200

    connect to Wifi

--- main.py
+++ main.py
@@ -3,7 +3,5 @@ import machine
 
 sl.schedule_on_cold_boot("my_module", "init_on_cold_boot")
 
-# sl.run_forever()
-
 if machine.wake_reason() is machine.DEEPSLEEP_RESET:
     sl.run_forever()
--- my_module.py
+++ my_module.py
@@ -1,14 +1,30 @@
 import sleepscheduler as sl
+import network
+from ntptime import settime
 import utime
 
 
 def init_on_cold_boot():
-    sl.schedule_at_sec("my_module", "scheduled_recurrently", 0, 10)
+    wlan = network.WLAN(network.STA_IF)
+    if not wlan.isconnected():
+        print("connecting to network")
+        wlan.active(True)
+        wlan.connect("yourSSID", "yourPassword")
+        while not wlan.isconnected():
+            print(".", end="")
+            utime.sleep(1)
+    print()
+    print('network config:', wlan.ifconfig())
+    settime()
+    print(utime.localtime())
+
+    sl.schedule_at_sec("my_module", "scheduled_recurrently", utime.time(), 10)
     # sl.allow_deep_sleep = False
 
 
 def scheduled_recurrently():
     print("scheduled_recurrently, time: " + str(utime.time()))
+    print(utime.localtime())
     utime.sleep(2)
-    print("print_tasks:")
-    sl.print_tasks()
+    # print("print_tasks:")
+    # sl.print_tasks()
