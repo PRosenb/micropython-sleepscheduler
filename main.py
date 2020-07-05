60bd82b1687e35b42ca1b0fb246e39e6dde7ca77
Peter Rosenberg <p.rosenberg@gmx.net>
2020-07-05 21:12:59 +0200

    run scheduler also on cold boot

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 import sleepscheduler as sl
-import machine
 
 sl.schedule_on_cold_boot("my_module", "init_on_cold_boot")
-
-if machine.wake_reason() is machine.DEEPSLEEP_RESET:
-    sl.run_forever()
+sl.run_forever()
