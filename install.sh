5f9a08d25c4250ac26bd0918199bbb5d320182b6
Peter Rosenberg <p.rosenberg@gmx.net>
2020-07-26 20:50:58 +0200

    adjust install.sh and .gitignore

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-ampy --port /dev/cu.SLAB_USBtoUART rm sleepscheduler.py
-ampy --port /dev/cu.SLAB_USBtoUART put sleepscheduler.py
-echo Installed
+# install ampy from https://learn.adafruit.com/micropython-basics-load-files-and-run-code/install-ampy
+ampy --port /dev/cu.SLAB_USBtoUART put main.py
+ampy --port /dev/cu.SLAB_USBtoUART put sleepscheduler/sleepscheduler.py
